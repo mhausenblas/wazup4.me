1275fa62b0b9150a81d64dd7d47295e8885e66b4
Michael Hausenblas <michael.hausenblas@gmail.com>
2013-04-08 10:04:16 +0100

    finalised fav handling via local storage

--- javascripts/wazup4me.js
+++ javascripts/wazup4me.js
@@ -1,11 +1,29 @@
+var w4mstorage = window.localStorage;
+
 $(function(){
-	$('.what,.break').click(function(){
+	initFavourites();
+	$('.what').click(function(){
+		var fav = W4M_EVENT_PREFIX + $(this).attr('id');
 		if($(this).hasClass('fav')){
 			$(this).removeClass('fav');
+			console.log('Removing favourite ' + fav);
+			w4mstorage.removeItem(fav);
 		}
 		else{
 			$(this).addClass('fav');
+			console.log('Adding favourite ' + fav);
+			w4mstorage.setItem(fav, '1');
 		}
 		return false;
 	});
 });
+
+
+function initFavourites(){
+	$('.what').each(function() {
+		var fav = W4M_EVENT_PREFIX + $(this).attr('id');
+		if(w4mstorage.getItem(fav) == '1' ){
+			$(this).addClass('fav');
+		}
+	});
+}
